f6b8ffbb46a78d077a31cb25054b16acfde9ca5e
Рязанов Е.А <sky655@gmail.com>
2018-11-16 19:30:58 +0400

    Add files

---
+++ PhoneNetworkApp/CellphoneNetwork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneNetworkApp
+{
+    public class CellphoneNetwork
+    {
+        private List<Person> personList;
+
+        public List<Person> PersonList { get => personList; set => personList = value; }
+    }
+}
---
+++ PhoneNetworkApp/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneNetworkApp
+{
+    public class Person
+    {
+        public Person()
+        {
+            name = "SampleName";
+            surname = "SampleSurname";
+            phoneNumber = "123456789";
+        }
+
+        private string name, surname, phoneNumber;
+
+        public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set => surname = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+    }
+}
